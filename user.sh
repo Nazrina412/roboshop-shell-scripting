20837a9e1dab45eb4478ac5d04f5a64255c7c4b3
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 17:25:56 -0400

    redis

--- user.sh
+++ user.sh
@@ -1,13 +1,3 @@
 source common.sh
-cp user.service /etc/systemd/system/user.service
+component=user
 Nodejs
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
