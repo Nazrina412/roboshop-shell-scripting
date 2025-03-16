37f98c1c73e3b879b9089682ef5ca4bba4588edf
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 18:41:58 -0400

    print process

--- common.sh
+++ common.sh
@@ -2,48 +2,48 @@ LOG_File=/tmp/roboshop.log
 
 Nodejs() {
   echo Disable Nodejs Default version
-  dnf module disable nodejs -y >/tmp/roboshop.&LOG_File
+  dnf module disable nodejs -y >&LOG_File
 
   echo enable Nodejs  version 20 module
-  dnf module enable nodejs:20 -y >/tmp/roboshop.&LOG_File
+  dnf module enable nodejs:20 -y >&LOG_File
 
   echo install NodeJs
   dnf install nodejs -y
 
   echo copy service file
-  cp ${component}.service /etc/systemd/system/${component}.service >/tmp/roboshop.&LOG_File
+  cp ${component}.service /etc/systemd/system/${component}.service >&LOG_File
 
  echo Copy mongodb repo file
- cp mongo.repo /etc/yum.repos.d/mongo.repo >/tmp/roboshop.&LOG_File
+ cp mongo.repo /etc/yum.repos.d/mongo.repo >&LOG_File
 
- echo install Nodejs >/tmp/roboshop.&LOG_File
+ echo install Nodejs >&LOG_File
  Nodejs
 
  echo add user
- useradd roboshop >/tmp/roboshop.&LOG_File
+ useradd roboshop >&LOG_File
 
  echo cleaning old content
- rm -rf /app >/tmp/roboshop.&LOG_File
+ rm -rf /app >&LOG_File
 
 
  echo create directory
- mkdir /app >/tmp/roboshop.&LOG_File
+ mkdir /app >&LOG_File
 
  echo download app file
- curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip >/tmp/roboshop.&LOG_File
+ curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip >&LOG_File
 
  echo go to directory
- cd /app >/tmp/roboshop.&LOG_File
+ cd /app >&LOG_File
 
 
  echo unzip file
- unzip /tmp/${component}.zip >/tmp/roboshop.&LOG_File
+ unzip /tmp/${component}.zip >&LOG_File
 
-cd /app >/tmp/roboshop.&LOG_File
+cd /app >&LOG_File
 
  echo starting the service
- npm install >/tmp/roboshop.&LOG_File
- systemctl daemon-reload >/tmp/roboshop.&LOG_File
- systemctl enable ${component} >/tmp/roboshop.&LOG_File
- systemctl start ${component} >/tmp/roboshop.&LOG_File
+ npm install >&LOG_File
+ systemctl daemon-reload >&LOG_File
+ systemctl enable ${component} >&LOG_File
+ systemctl start ${component} >&LOG_File
 }
