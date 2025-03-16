709fed47a8062bd71c0f78e33756390dfa9dfe95
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 15:05:57 -0400

    redis

--- rabbit.sh
+++ rabbit.sh
@@ -2,6 +2,5 @@ cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
-
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
