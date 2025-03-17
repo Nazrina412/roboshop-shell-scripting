326bcbaf9b0920df970ef8ebca74b93ee1c4bf04
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 21:16:07 -0400

    print process

--- redis.sh
+++ redis.sh
@@ -1,23 +1,24 @@
-
+source common.sh
+component=redis
 
 PRINT Disable redis default
-dnf module disable redis -y
+dnf module disable redis -y &>>LOG_File
 STAT $?
 
 PRINT Enable redis
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>LOG_File
 STAT $?
 
 PRINT Install Redis
-dnf install redis -y
+dnf install redis -y &>>LOG_File
 STAT $?
 
 PRINT Update redis Config
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>LOG_File
+sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>LOG_File
 STAT $?
 
 PRINT Start Redis service
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis &>>LOG_File
+systemctl restart redis &>>LOG_File
 STAT $?
