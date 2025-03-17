a8f76973561837fab96548f41798113c34845d4c
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 21:08:49 -0400

    print process

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,19 @@
+source common.sh
+component=mongo
+
+PRINT Copy repo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+STAT $?
+
+PRINT Install mongodb
 dnf install mongodb-org -y
+STAT $?
+
+PRINT update config file
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+PRINT Start mongodb service
 systemctl enable mongod
 systemctl restart mongod
-##
+STAT $?
