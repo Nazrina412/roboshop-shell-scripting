c4f92e2ef8ae14f7b4269c19b5348a785cf95a81
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 20:33:50 -0400

    print process

--- common.sh
+++ common.sh
@@ -13,7 +13,7 @@ STAT() {
         echo -e "\e[32mSUCCESS\e[0m"
         else
           echo -e "\e[31mFAILURE\e[0m"
-          exit $1
+          exit $1 "Refer the log file for more information"
           fi
 
 }
