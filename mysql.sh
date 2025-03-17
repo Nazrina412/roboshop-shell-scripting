ae36b8d2303cb638e75b8c425a08a642ad7d39e9
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 21:54:24 -0400

    print process

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,15 @@
+source common.sh
+component=mysl
+
+PRINT MYSQL INSTALL
 dnf install mysql-server -y
+STAT $?
+
+PRINT MYSQL ENABLE
 systemctl enable mysqld
+STAT $?
+
+PRINT Start service
 systemctl start mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
+STAT $?
