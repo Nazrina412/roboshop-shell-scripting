60cffae22861f1c40abb5fc071476942ce61c1df
Nazrina <nazrinakhan88@gmail.com>
2025-03-16 21:14:28 -0400

    print process

--- redis.sh
+++ redis.sh
@@ -1,7 +1,23 @@
+
+
+PRINT Disable redis default
 dnf module disable redis -y
+STAT $?
+
+PRINT Enable redis
 dnf module enable redis:7 -y
+STAT $?
+
+PRINT Install Redis
 dnf install redis -y
+STAT $?
+
+PRINT Update redis Config
 sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT Start Redis service
 systemctl enable redis
 systemctl restart redis
+STAT $?
